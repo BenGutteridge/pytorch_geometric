3451737ec0fd60321f467dd672928501bd22b2a6
BenGutteridge <benpgutteridge@gmail.com>
2023-05-12 19:28:31 +0100

    reproducing ringtransfer

--- graphgym/get_ringtransfer.sh
+++ graphgym/get_ringtransfer.sh
@@ -3,14 +3,14 @@
 # define an array of numbers
 layers=(5 10 15 20 30 40 50 60)
 
-# stage=stack
-# layer=gcnconv
+stage=stack
+layer=gcnconv
 
 # stage=delay_gnn
 # layer=my_gcnconv
 
-stage=k_gnn
-layer=my_gcnconv
+# stage=k_gnn
+# layer=my_gcnconv
 
 d=256
 
@@ -18,5 +18,5 @@ d=256
 for L in "${layers[@]}"
 do
   dir="results/og_cfg/${stage}_L=${L}"
-  python main.py --cfg /Users/beng/Documents/pytorch_geometric/graphgym/configs/pre_dec_22/ring_transfer.yaml --repeat 3 gnn.layers_mp $L gnn.layer_type $layer gnn.stage_type $stage out_dir $dir
+  python3.8 main.py --cfg /Users/beng/Documents/pytorch_geometric/graphgym/configs/pre_dec_22/ring_transfer.yaml --repeat 3 gnn.layers_mp $L gnn.layer_type $layer gnn.stage_type $stage out_dir $dir
 done
